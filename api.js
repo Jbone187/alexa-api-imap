53e942c5a134d84acec6fa7d09743fed0c7907aa
Jasen Green <jasen.green@protonmail.com>
2020-07-29 08:51:59 -0700

    Update api.js

--- api.js
+++ api.js
@@ -1,56 +1,67 @@
-const notifier = require('mail-notifier');
+ const notifier = require('mail-notifier');
 
-const https = require("https");
+ const https = require("https");
 
-console.log('Api is Running');
+ console.log('Api is Running');
 
-let body = JSON.stringify({
-"notification": "",
-"accessCode": ""
-});
+ try {
 
-const imap = {
-user: "",
-password: "",
-host: "",
-port: 993, // imap port
-tls: true,// use secure connection
-tlsOptions: { rejectUnauthorized: false }
-};
+     let body = JSON.stringify({
+         "notification": "Person has been identified",
+         "accessCode": ""
+     });
 
+     const imap = {
+         user: "",
+         password: "",
+         host: "",
+         port: 993, // imap port
+         tls: true, // use secure connection
+         tlsOptions: {
+             rejectUnauthorized: false
+         }
 
-notifier(imap)
-.on('mail', mail => console.log(mail)).start();
-
-
-const n = notifier(imap);
-
-n.on('end', () => n.start()).on('mail', function(mail){
-
-if(mail.subject === "Activity Alert: Person detected"){
-
-console.log(mail.from[0].address, mail.subject)
-
-let request = https.request({
-hostname: "api.notifymyecho.com",
-path: "/v1/NotifyMe",
-method: "POST",
-headers: {
-"Content-Type": "application/json",
-"Content-Length": Buffer.byteLength(body)
-}
-});
-
-request.on('error', function(err) {
-console.log(err);
-});
-
-request.end(body);
-
-}else{
-
-console.log("Waiting on Data");
-};
+     };
 
 
-}).start();
+
+     notifier(imap)
+         .on('mail', mail => console.log(mail)).start();
+
+
+     const n = notifier(imap);
+
+     n.on('end', () => n.start()).on('mail', function (mail) {
+
+         if (mail.subject === "Activity Alert: Arlo Smart Detected a Person") {
+
+             console.log(mail.from[0].address, mail.subject)
+
+             let request = https.request({
+                 hostname: "api.notifymyecho.com",
+                 path: "/v1/NotifyMe",
+                 method: "POST",
+                 headers: {
+                     "Content-Type": "application/json",
+                     "Content-Length": Buffer.byteLength(body)
+                 }
+             });
+
+             request.on('error', function (err) {
+                 console.log(err);
+             });
+
+             request.end(body);
+
+         } else {
+
+             console.log("Waiting on Data");
+         };
+
+
+     }).start();
+
+ } catch (error) {
+    console.error(error);
+
+ };
